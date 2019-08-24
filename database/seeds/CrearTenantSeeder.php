5293a1a10522f3682b2e16ff8147ca60ffa1a05b
gerchavarin <gerchavarin@gmail.com>
2019-08-23 18:38:44 -0600

    initial commit

---
+++ database/seeds/CrearTenantSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Hyn\Tenancy\Contracts\Repositories\HostnameRepository;
+use Hyn\Tenancy\Contracts\Repositories\WebsiteRepository;
+use Hyn\Tenancy\Environment;
+use Hyn\Tenancy\Models\Hostname;
+use Hyn\Tenancy\Models\Website;
+
+class CrearTenantSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $website = new Website;
+        $website->uuid = "tenant_test_1";
+        $website->managed_by_database_connection = 'tenant1';
+        app(WebsiteRepository::class)->create($website);
+        
+        $hostname = new Hostname;
+        $hostname->fqdn = 'tenant1.demo.test';
+        $hostname = app(HostnameRepository::class)->create($hostname);
+        app(HostnameRepository::class)->attach($hostname, $website);
+        
+        $hostname = app(\Hyn\Tenancy\Environment::class)->hostname();
+        $website = app(\Hyn\Tenancy\Environment::class)->website();
+        
+        $website = new Website;
+        $website->uuid = "tenant_test_2";
+        $website->managed_by_database_connection = 'tenant2';
+        app(WebsiteRepository::class)->create($website);
+        
+        $hostname = new Hostname;
+        $hostname->fqdn = 'tenant2.demo.test';
+        $hostname = app(HostnameRepository::class)->create($hostname);
+        app(HostnameRepository::class)->attach($hostname, $website);
+        
+        $hostname = app(\Hyn\Tenancy\Environment::class)->hostname();
+    	$website = app(\Hyn\Tenancy\Environment::class)->website();
+    }
+}
---
+++ routes/tenants.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::middleware('web')
+    ->namespace('App\\Http\\Controllers\\')
+    ->group(function ()
+{
+    Route::get('/hello', function () {
+        return '<h1>Hello Tenant</h1>';
+    });
+    
+    Route::get('/users', function() {
+        dump(app('request')->url());
+        dump(App\User::all()->pluck('name'));
+    });
+});
