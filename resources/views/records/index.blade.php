2a9afa2a317f51dc9a8fb7ca40e99767961f224b
Victor Santana <isai@localhost.localdomain>
2019-09-03 22:19:48 -0500

    Records functionality was added v beta

---
+++ resources/views/records/index.blade.php
@@ -0,0 +1,53 @@
+@extends('records.layout')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    records
+                    <a href="{{ route('records.create')}}" class="btn btn-success btn-sm float-right">Crear</a>
+                    
+                </div>
+                
+                <div class="card-body">
+                    @if (session('success'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('success') }}
+                        </div>
+                    @endif
+
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                            <td>RFC</td>
+                            <td>Descripción</td>
+                            <td>Nombre</td>
+                            <td colspan="2">Action</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($records as $record)
+                            <tr>
+                                <td>{{$record->type}}</td>
+                                <td>{{$record->mount}}</td>
+                                <td>{{$record->description}}</td>
+                                <td><a href="{{ route('records.edit',$record->id)}}" class="btn btn-primary btn-sm">Edit</a></td>
+                                <td>
+                                    <form action="{{ route('records.destroy', $record->id)}}" method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn btn-danger btn-sm" type="submit">Delete</button>
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
